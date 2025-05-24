e061040ca8377c3d852ad163b6bbf41b8dad8852
ImjaeMVP <yesa1504@gmail.com>
2025-05-24 20:48:44 +0900

    #fix : change button layout

--- app/src/main/java/kr/co/gachon/moproject_d/InitialAccountActivity.java
+++ app/src/main/java/kr/co/gachon/moproject_d/InitialAccountActivity.java
@@ -1,6 +1,11 @@
 package kr.co.gachon.moproject_d;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +16,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class InitialAccountActivity extends AppCompatActivity {
 
-
+    Button btnComplete;
+    EditText txtAccountID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,24 @@ public class InitialAccountActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnComplete = findViewById(R.id.btn_initial_account_complete);
+        txtAccountID = findViewById(R.id.txt_initial_user_id);
+
+
+        btnComplete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtAccountID.getText().toString().equals("TEXT INPUT")){
+                    Toast.makeText(getApplicationContext(),"다시 입력하시오", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Intent intent = new Intent(InitialAccountActivity.this, InitialCompleteActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
 
 
     }
