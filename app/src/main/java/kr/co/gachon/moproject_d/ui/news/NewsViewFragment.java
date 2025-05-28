939ca111f50f2e03aebce70443a14bb901730405
ImjaeMVP <yesa1504@gmail.com>
2025-05-28 14:35:16 +0900

    #feat : add button click and add variety

--- app/src/main/java/kr/co/gachon/moproject_d/ui/news/NewsViewFragment.java
+++ app/src/main/java/kr/co/gachon/moproject_d/ui/news/NewsViewFragment.java
@@ -5,6 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -28,10 +32,23 @@ public class NewsViewFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_news_view, container, false);
 
         Button btnBack = view.findViewById(R.id.btn_news_back);
+        ImageButton btnWord = view.findViewById(R.id.btn_news_view_word);
+
+        TextView txtNewsTitle = view.findViewById(R.id.txt_news_view_title);
+        TextView txtNewsContent = view.findViewById(R.id.txt_news_view_content);
+        TextView txtNewsWord = view.findViewById(R.id.txt_news_view_word);
+        TextView txtNewsWordDescription = view.findViewById(R.id.txt_news_view_description);
+
+        ImageView imgNewsImg = view.findViewById(R.id.img_news_view_img);
+
 
         btnBack.setOnClickListener(v -> {
             NavController navController = NavHostFragment.findNavController(this);
             navController.popBackStack();
+        });
+
+        btnWord.setOnClickListener(v -> {
+            Toast.makeText(requireContext(),"구현 예정(데이터베이스필요)", Toast.LENGTH_LONG).show();
         });
 
 
