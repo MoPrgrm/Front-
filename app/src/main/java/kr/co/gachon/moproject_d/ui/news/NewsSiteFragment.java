cf2a8b091006ece9de564900b7f78e5569cd03d6
ImjaeMVP <yesa1504@gmail.com>
2025-05-28 14:35:36 +0900

    #feat : add button click

--- app/src/main/java/kr/co/gachon/moproject_d/ui/news/NewsSiteFragment.java
+++ app/src/main/java/kr/co/gachon/moproject_d/ui/news/NewsSiteFragment.java
@@ -28,6 +28,8 @@ public class NewsSiteFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_news_select_site, container, false);
 
         Button btnCNN = view.findViewById(R.id.btn_cnn);
+        Button btnBBC = view.findViewById(R.id.btn_bbc);
+        Button btnREUTERS = view.findViewById(R.id.btn_reuters);
         Button btnBack = view.findViewById(R.id.btn_news_back);
 
         btnBack.setOnClickListener(v -> {
@@ -36,6 +38,14 @@ public class NewsSiteFragment extends Fragment {
         });
 
         btnCNN.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_news_site_to_topic);
+        });
+
+        btnBBC.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_news_site_to_topic);
+        });
+
+        btnREUTERS.setOnClickListener(v -> {
             Navigation.findNavController(v).navigate(R.id.action_news_site_to_topic);
         });
 
